f3f0f416b074319b69c9a0dd3d883a1bd4b537a8
watarmeli <watar@ukr.net>
2021-04-28 03:21:05 +0300

    timer is ready

---
+++ index.js
@@ -0,0 +1,55 @@
+const refs = {
+    timerIndicator: document.querySelector('#timer-1'),
+    days: document.querySelector('span[data-value="days"]'),
+    hours: document.querySelector('span[data-value="hours"]'),
+    mins: document.querySelector('span[data-value="mins"]'),
+    secs: document.querySelector('span[data-value="secs"]'),
+}
+
+
+
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+    };
+
+    start() {
+        setInterval(() => {            
+            const currentDate = Date.now();
+            const timeLeft = this.targetDate - currentDate;
+            const time = this.getTimeComponents(timeLeft);
+            this.updateTimer(time);
+        }, 1000)
+    };
+
+    
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));          
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        return { days, hours, mins, secs };
+    };
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    };
+
+    updateTimer({ days, hours, mins, secs }) {
+        refs.days.textContent = `${days}`;
+        refs.hours.textContent = `${hours}`;
+        refs.mins.textContent = `${mins}`;
+        refs.secs.textContent = `${secs}`;
+    };
+};
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 31, 2021'),
+});
+
+timer.start();
+
